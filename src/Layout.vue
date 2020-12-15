6dea18b7de643ea69d2561957fd9f0a61505ab0c
caojen <caojen@mail2.sysu.edu.cn>
2020-12-16 01:21:46 +0800

    初始化页面

---
+++ src/Layout.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <a-layout-header class="header">
+      <div class="logo" />
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider
+        width="200"
+        style="background: #fff"
+        v-model="collapsed"
+        collapsible
+        theme="light"
+        v-if=false
+      >
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '500px' }"
+        >
+          <router-view />
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      collapsed: false
+    }
+  }
+}
+</script>
+
+<style>
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
+</style>
