dea8e7132d2bfdeae4311a9cac1427ebfe5e213a
caojen <caojen@mail2.sysu.edu.cn>
2020-12-18 21:18:14 +0800

    修改图标和标题

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import Meta from 'vue-meta'
 
 Vue.use(Antd)
+Vue.use(Meta)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  metaInfo () {
+    return {
+      title: '计算机学院研究生管理系统'
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
